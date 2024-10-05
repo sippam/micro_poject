d8670213fa45d5c852ff1ff98bcb1c9ef9a93369
sippam <sippakorn.wo@kkumail.com>
2024-10-05 22:21:08 +0700

    [Change] backend to socket

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,40 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const WebSocket = require("ws");
+const app = express();
+const bodyParser = require("body-parser");
 
-const PORT = 3001
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World'))
+const wss = new WebSocket.Server({ port: 8080 });
+
+const PORT = 3001;
+
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+
+  ws.on("message", (message) => {
+    console.log(`Received message => ${message}`);
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+app.post("/api/send", (req, res) => {
+  const { sensor_value } = req.body;
+
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify({ sensorValue: sensor_value, tem: 25 }));
+        }
+      });
+      
+  res.status(200).send("Data received");
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port : ${PORT}`)
-})
-module.exports = app
+  console.log(`Server is running on port : ${PORT}`);
+});
+
+module.exports = app;
